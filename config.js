ceddd8e9943673392c0539103d6cde6abff034ab
ducthanh <ducthanhpg@gmail.com>
2022-02-24 11:35:20 +0700

    first

---
+++ config.js
@@ -0,0 +1,18 @@
+const CONFIG = {
+    introTitle: 'Bé à!',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Vẫn cứ là thích anh <33',
+    btnNo: 'Không, Anh à :3',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Bởi vì anh đẹp trai dễ thương không iu anh thì iu ai 😘😘',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    btnAccept: 'Yêu <3',
+    messLink: 'https://www.facebook.com/messages/t/ducthanhpg1' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
